6e03fd5d2bdf0f2ffd523b1c082c2100ac3d5734
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-02-09 14:06:21 +0900

    [start-finish]3-8,9

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ac4bad43480afe5a8b023296e44fc19b1ea693b3f4d5417ff02531a328a8177a4c9bf2e242ae683a3e6a2c0c71a0495e88db8d7ea82c4c0d5f70be772b45b8f0'
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_103020) do
+ActiveRecord::Schema.define(version: 2020_02_08_120056) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
